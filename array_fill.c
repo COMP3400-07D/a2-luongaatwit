6372a7a9c5eb9e419be4a459ae69a92b36444b5a
luongaatwit <luonga@wit.edu>
2025-10-09 12:29:30 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -1,8 +1,20 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * fill array with interger numbers in increasing order
+ * @param array the array to fill
+ * @param array_len length of array
+ * @param begin the starting number of array
+ * @param end the ending number of array
+ * @return numbers filled into array
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    
+    int count = 0;
+    
+    for (int num = begin; num <=end && count < array_len; num++) {
+        array[count] = num;
+        count++;
+
+    }
 }
